64e63d87cf090d9fca4697a61be98d8054b232f5
noemie <noemiegagnon23@gmail.com>
2021-05-17 22:17:38 -0400

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function Header() {
+    return (
+        <header>
+            <h1 className="title">Meme Generator</h1>
+        </header>
+    )
+}
+
+export default Header
